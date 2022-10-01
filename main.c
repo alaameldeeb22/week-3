02094d174bb7ea32739f94b37618126bf68e66f7
alaameldeeb22 <105177463+alaameldeeb22@users.noreply.github.com>
2022-10-01 04:02:26 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,60 +1,21 @@
 #include <stdio.h>
-
-void swap(int *a, int *b) {
-    int t = *a;
-    *a = *b;
-    *b = t;
-}
-int quick(int array[], int low, int high) {
-
-    int pivot = array[high];
-
-    int i = low-1 ;
-
-    for (int j = low; j < high; j++) {
-        if (array[j] <= pivot) {
-            i++;
-            swap(&array[i], &array[j]);
-
-        }
-    }
-    swap(&array[i + 1], &array[high]);
-
-    return (i + 1);
-}
-
-void quickSort(int array[], int low, int high) {
-    if (low < high) {
-
-         int pi = quick(array, low, high);
-
-        // recursive call on the left of pivot
-        quickSort(array, low, pi - 1);
-
-        // recursive call on the right of pivot
-        quickSort(array, pi + 1, high);
-    }
-}
-
-
-void printArray(int A[], int size)
-{
-    int i;
-    for (i = 0; i < size; i++)
-        printf("%d ", A[i]);
-    printf("\n");
-}
-
-
 int main()
 {
+    int pos;
     int arr[] = { 12, 11, 13, 5, 6, 7 };
-    int arr_size = sizeof(arr) / sizeof(arr[0]);
-    printf("Unsorted Array\n");
-    printArray(arr, arr_size);
-    quickSort(arr, 0, arr_size - 1);
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printf("Enter the element position to delete : ");
+    scanf("%d", &pos);
+    for(int i=pos-1; i<n-1; i++)
+    {
+        arr[i] = arr[i + 1];
+    }
+    n--;
+    printf("\nElements of array after delete are : ");
+    for(int i=0; i<n; i++)
+    {
+        printf("%d\t", arr[i]);
+    }
 
-    printf("\nSorted array is \n");
-    printArray(arr, arr_size);
     return 0;
 }
