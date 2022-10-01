f1ce67f4b73a66268c0a116779961160ba5b4a99
alaameldeeb22 <105177463+alaameldeeb22@users.noreply.github.com>
2022-10-01 03:42:14 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,38 +1,59 @@
 #include <stdio.h>
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
 
-void swap(int *a, int *b) {
-    int t = *a;
-    *a = *b;
-    *b = t;
-}
-int quick(int array[], int low, int high) {
+    int L[n1], R[n2];
 
-    int pivot = array[high];
 
-    int i = low-1 ;
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
 
-    for (int j = low; j < high; j++) {
-        if (array[j] <= pivot) {
+
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
             i++;
-            swap(&array[i], &array[j]);
-
         }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
     }
-    swap(&array[i + 1], &array[high]);
 
-    return (i + 1);
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
 }
 
-void quickSort(int array[], int low, int high) {
-    if (low < high) {
 
-         int pi = quick(array, low, high);
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r) {
 
-        // recursive call on the left of pivot
-        quickSort(array, low, pi - 1);
+        int m = l + (r - l) / 2;
 
-        // recursive call on the right of pivot
-        quickSort(array, pi + 1, high);
+
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
     }
 }
 
@@ -50,9 +71,8 @@ int main()
 {
     int arr[] = { 12, 11, 13, 5, 6, 7 };
     int arr_size = sizeof(arr) / sizeof(arr[0]);
-    printf("Unsorted Array\n");
-    printArray(arr, arr_size);
-    quickSort(arr, 0, arr_size - 1);
+
+    mergeSort(arr, 0, arr_size - 1);
 
     printf("\nSorted array is \n");
     printArray(arr, arr_size);
